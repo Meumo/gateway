6b64249c404dd5554cd583b185896765e32d2464
Meumo <mamadou-d@hotmail.com>
2022-05-13 19:04:09 +0000

    First commit

---
+++ src/test/java/sn/meum/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.meum.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
